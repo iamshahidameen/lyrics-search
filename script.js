438e86140bb72f876e3b0f625ccba6b0c7395237
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-04-18 23:59:28 +0500

    updated js file

--- script.js
+++ script.js
@@ -1,66 +1,93 @@
-const form = document.getElementById('form'),
-search = document.getElementById('search'),
-result = document.getElementById('result'),
-more = document.getElementById('more'),
-apiURL = 'https://api.lyrics.ovh';
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
 
-//  Search Songs with API
+const apiURL = 'https://api.lyrics.ovh';
+
+// Search by song or artist
 async function searchSongs(term) {
-    const res = await fetch(`${apiURL}/suggest/${term}`);
-    const data = await res.json();
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
 
-    //  Calling ShowData to show data on DOM
-
-    showData(data);
+  showData(data);
 }
 
-//  Show Song Name and Artist Name in DOM
+// Show song and artist in DOM
+function showData(data) {
+  result.innerHTML = `
+    <ul class="songs">
+      ${data.data
+        .map(
+          song => `<li>
+      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    </li>`
+        )
+        .join('')}
+    </ul>
+  `;
 
-function showData(LyricsResults){
-    let output = '';
-
-    // First method with forEach
-
-    /*LyricsResults.data.forEach( song => {
-        output += `
-            <li>
-                <span>
-                    <strong>${song.artist.name}</strong> - ${song.title}
-                </span>
-                <button class="btn" data-artist="$${song.artist.name}" data-song="${song.title}">Get Lyrics</button>
-            </li>
-        `;
-
-        result.innerHTML = `
-        <ul class="songs">${output}</ul>
-        `;
-    })*/
-
-    //Second Method with map()
-    result.innerHTML = `
-        <ul class="songs">
-            ${LyricsResults.data.map(song => `
-                <li>
-                    <span>
-                        <strong>${song.artist.name}</strong> - ${song.title}
-                    </span>
-                    <button class="btn" data-artist="$${song.artist.name}" data-song="${song.title}">Get Lyrics</button>
-                </li>
-            `).join()}
-        </ul>
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ''
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+          : ''
+      }
     `;
-
+  } else {
+    more.innerHTML = '';
+  }
 }
 
-//  Form where user puts the serach term
+// Get prev and next songs
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`;
+
+  more.innerHTML = '';
+}
+
+// Event listeners
 form.addEventListener('submit', e => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const searchTerm = search.value.trim();
+  const searchTerm = search.value.trim();
 
-    if (!searchTerm) {
-        alert('Please type in a search term');
-    } else {
-        searchSongs(searchTerm);
-    }
-})
+  if (!searchTerm) {
+    alert('Please type in a search term');
+  } else {
+    searchSongs(searchTerm);
+  }
+});
+
+// Get lyrics button click
+result.addEventListener('click', e => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
+  }
+});
